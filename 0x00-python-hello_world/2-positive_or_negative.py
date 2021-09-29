b984947277534a506599064912323e011d94e66b
ttkeele <3264@holbertonschool.com>
2021-09-29 17:52:48 +0000

    changed if statements

--- 0x00-python-hello_world/2-positive_or_negative.py
+++ 0x00-python-hello_world/2-positive_or_negative.py
@@ -2,9 +2,9 @@
 import random
 number = random.randint(-10, 10)
 
-if number < 0:
-        print("{} is negative".format(number))
+if number > 0:
+            print(number, "is positive")
 elif number == 0:
-        print("{} is zero".format(number))
-else:
-        print("{} is positive".format(number))
+            print(number, "is zero")
+elif number < 0:
+            print(number, "is negative")
