e1496f9cb29ee5c60f9889ced45eda127c92462a
ttkeele <3264@holbertonschool.com>
2021-09-29 15:57:59 +0000

    This program will assign a random signed number to the variable number each time it is executed. Complete the source code in order to print the last digit of the number stored in the variable number

--- 0x00-python-hello_world/3-last_digit.py
+++ 0x00-python-hello_world/3-last_digit.py
@@ -1,16 +1,13 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-zero = "0"
-great5 = "greater than 5"
-less6 = "less than 6 and not 0"
-if number > 0:
-        last = number % 10
+modnum = number % 10 if number > 0 else number % -10
+if modnum > 5:
+            print("Last digit of {} is {} and is greater than 5".format(
+                            number, modnum))
+elif modnum == 0:
+            print("Last digit of {} is {} and is 0".format(
+                            number, modnum))
 else:
-        last = number % -10
-        if last > 5:
-                print("Last digit of {} is {} and is {}".format(number, last, great5))
-        elif last == 0:
-                print("Last digit of {} is {} and is {}".format(number, last, zero))
-        else:
-                print("Last digit of {} is {} and is {}".format(number, last, less6))
+            print("Last digit of {} is {} and is less than 6 and not 0".format(
+                            number, modnum))
