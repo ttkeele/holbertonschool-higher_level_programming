17698ae3c875304d0caab08019b6c8ed4ac9b3ed
ttkeele <3264@holbertonschool.com>
2021-09-30 20:55:02 +0000

    fixed array

--- 0x02-python-data_structures/9-max_integer.py
+++ 0x02-python-data_structures/9-max_integer.py
@@ -2,7 +2,7 @@
 def max_integer(my_list=[]):
     if my_list:
         result = my_list[0]
-        for i in my_list[1:]:
+        for i in my_list:
             if i > result:
                 result = i
             return result
