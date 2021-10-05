d4e80b128a7721aed746f77887f8a2c901ebdf08
ttkeele <3264@holbertonschool.com>
2021-10-05 14:32:14 +0000

    class Square that defines a square by: (based on 0-square.py)

--- 0x04-python-classes/1-square.py
+++ 0x04-python-classes/1-square.py
@@ -1,17 +1,23 @@
 #!/usr/bin/python3
-"""Square class definition"""
+"""defines a class Square"""
 
 
 class Square:
     """Represents a square
-
     Attributes:
     __size (int): size of a side of the square
     """
-    def __init__(self, size):
-        """Initializes a square
+    def __init__(self, size=0):
+        """initializes the square
         Args:
             size (int): size of a side of the square
-        Returns: None
+        Returns:
+            None
         """
-        self.__size = size
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        else:
+            if size < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = size
