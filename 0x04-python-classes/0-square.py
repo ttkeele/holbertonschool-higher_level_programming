69f19b99c08719634b6cb2515df08f3427aabc37
ttkeele <3264@holbertonschool.com>
2021-10-05 14:34:44 +0000

    class Square that defines a square

--- 0x04-python-classes/0-square.py
+++ 0x04-python-classes/0-square.py
@@ -1,7 +1,16 @@
 #!/usr/bin/python3
-"""Filename:  0-square"""
+"""Square class definition"""
 
 
 class Square:
-    """An empty class Square that defines a square:"""
-    pass
+    """Represents a square
+    Attributes:
+        __size (int): size of a side of the square
+    """
+    def __init__(self, size):
+        """Initializes a square
+        Args:
+            size (int): size of a side of the square
+        Returns: None
+        """
+        self.__size = size
