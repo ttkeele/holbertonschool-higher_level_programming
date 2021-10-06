3085540759cc397829a37775399736533e90464d
ttkeele <3264@holbertonschool.com>
2021-10-06 12:46:08 +0000

    fixed indents

--- 0x04-python-classes/2-square.py
+++ 0x04-python-classes/2-square.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
-"""Defines a class Square"""
+"""Define a class Square"""
 
 
 class Square:
-    """Define size with verification"""
+    """Represent a square"""
 
     def __init__(self, size=0):
-        self.__size = size
-        if type(size) is not int:
-            raise TypeError("size must be an integer")
-        if size < 0:
-                raise ValueError("size must be >= 0")
-        """Define area of square"""
+        """Initialize a new Square
 
-        def area)self, size=0):
-            sq_area = size.__size ** 2
-            return sq__area
+        Args:
+            size (int): The size of the new square
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
