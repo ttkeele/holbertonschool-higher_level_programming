b0beca103c2f312e7763e507b19d9ee57a8a0106
ttkeele <3264@holbertonschool.com>
2021-10-12 16:50:55 +0000

    repr() should return a string representation of the rectangle to be able to recreate a new instance by using eval()

---
+++ 0x06-python-more_classes/3-rectangle.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python3
+"""This module defines a rectangle"""
+
+
+class Rectangle:
+    """Defines the rectangle class"""
+
+    def __init__(self, width=0, height=0):
+        if type(width) is not int:
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = width
+
+        if type(height) is not int:
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
+
+    def area(self):
+        return self.__width * self.__height
+
+    def perimeter(self):
+        if self.__height == 0 or self.__width == 0:
+            return 0
+        return (2 * self.__width) + (2 * self.__height)
+
+    def __str__(self):
+        newString = ""
+        if self.__height == 0 or self.__width == 0:
+            return newString
+        for HEIGHT in range(self.height):
+            for WIDTH in range(self.width):
+                    newString += "#"
+            newString += "\n"
+        newString = newString[:-1]
+        return newString
+
+    def __repr__(self):
+        return "Rectangle({:d}, {:d})".format(self.__width, self.__height)
