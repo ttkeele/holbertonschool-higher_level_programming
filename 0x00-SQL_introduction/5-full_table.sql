76a4e596a233269ab8251cbac0cddfbafdd9b194
ttkeele <3264@holbertonschool.com>
2021-11-02 23:23:51 +0000

    prints description of first_table without using describe or explain

---
+++ 0x00-SQL_introduction/5-full_table.sql
@@ -0,0 +1,5 @@
+-- prints description of the first_table from the database in SQL server
+-- The database name will be passed as an argument of the mysql command
+-- You are not allowed to use the DESCRIBE or EXPLAIN statements
+
+SHOW CREATE TABLE first_table;
