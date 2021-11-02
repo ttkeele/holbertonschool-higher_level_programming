3ea44d8a257441f2ac4fd003cc213593637265ea
ttkeele <3264@holbertonschool.com>
2021-11-02 23:40:59 +0000

    displays number of records with id=89

---
+++ 0x00-SQL_introduction/8-count_89.sql
@@ -0,0 +1,4 @@
+-- displays the number of records with id = 89 in first_table of database
+-- The database name will be passed as an argument of the mysql command
+
+SELECT COUNT(*) FROM first_table WHERE id=89;
