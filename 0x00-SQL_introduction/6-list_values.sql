189114894823b87415ffd6d4548c4cb83f136734
ttkeele <3264@holbertonschool.com>
2021-11-02 23:27:25 +0000

    lists all rows of first_table, all fields printed

---
+++ 0x00-SQL_introduction/6-list_values.sql
@@ -0,0 +1,5 @@
+-- lists all rows of first_table from the database in the  SQL server
+-- All fields should be printed
+-- The database name will be passed as an argument of the mysql command
+
+SELECT * FROM first_table;
