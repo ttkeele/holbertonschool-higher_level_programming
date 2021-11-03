c70abf04bf530c3609996b623c5f52bc4d23359a
ttkeele <3264@holbertonschool.com>
2021-11-03 00:23:54 +0000

    lists all records second_table, without listing rows with no name value, score must be displayed with name in oreder

---
+++ 0x00-SQL_introduction/16-no_link.sql
@@ -0,0 +1,7 @@
+-- lists all records of second_table
+-- Don’t list rows without a name value
+-- Results should display the score and the name (in this order)
+-- Records should be listed by descending score
+-- The database name will be passed as an argument to the mysql command
+
+SELECT score, name FROM second_table WHERE name IS NOT NULL AND name != '' ORDER BY score DESC;
