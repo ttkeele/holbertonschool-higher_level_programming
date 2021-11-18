ed84535620e598f8aa5b46eba856b5ff85b22c37
ttkeele <3264@holbertonschool.com>
2021-11-18 17:05:44 +0000

    fixed output

--- 0x0B-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0B-python-object_relational_mapping/11-model_state_insert.py
@@ -10,29 +10,15 @@ from model_state import Base, State
 
 
 if __name__ == "__main__":
-
-    username = argv[1]
-    passwd = argv[2]
-    db = argv[3]
-
-    engine_string = "mysql://{}:{}@localhost:3306/{}".format(username,
-                                                             passwd, db)
-
-    engine = create_engine(engine_string)
-    Base.metadata.bind = engine
-
-    DBSession = sessionmaker(bind=engine)
-    session = DBSession()
-
-    new_state = State(name="Louisiana")
+    eng = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1],
+                                                                    argv[2],
+                                                                    argv[3]))
+    Base.metadata.create_all(eng)
+    Session = sessionmaker(bind=eng)
+    session = Session()
+    new_state = State(name='Louisiana')
     session.add(new_state)
+    state = session.query(State).filter_by(name='Louisiana').first()
+    print(str(state.id))
     session.commit()
-
-    list = session.query(State).order_by(State.id).all()
-    found_flag = 0
-    for obj in list:
-        if obj.name == 'Louisiana':
-            print("{}".format(obj.id))
-            found_flag = 1
-        if found_flag == 0:
-            print("Not found")
+    session.close()
