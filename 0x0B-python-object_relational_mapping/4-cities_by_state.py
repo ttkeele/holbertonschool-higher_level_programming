314f5ba503adb264ac411971e95f4784f35752e2
ttkeele <3264@holbertonschool.com>
2021-11-18 00:35:38 +0000

    fixed cities

--- 0x0B-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0B-python-object_relational_mapping/4-cities_by_state.py
@@ -13,10 +13,10 @@ if __name__ == "__main__":
                          passwd=argv[2],
                          db=argv[3])
     cur = db.cursor()
-    cursor.execute("SELECT cities.id, cities.name, states.name FROM cities \
-    JOIN states ON cities.state_id = states.id ORDER BY cities.id")
+    cursor.execute("SELECT cities.name FROM cities \
+    JOIN states ON cities.state_id = states.id WHERE states.name LIKE %s \
+    ORDER BY cities.id", (argv[4],))
     rows = cursor.fetchall()
-    for row in rows:
-        print(row)
+    print(", ".join(city[0] for city in rows))
     cursor.close()
     db.close()
