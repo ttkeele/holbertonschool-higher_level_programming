eb201b98be49817ac086b5b5cc4ca6f27e0a6b66
ttkeele <3264@holbertonschool.com>
2021-11-18 16:53:04 +0000

    fixed query

--- 0x0B-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0B-python-object_relational_mapping/4-cities_by_state.py
@@ -13,10 +13,13 @@ if __name__ == "__main__":
                          passwd=argv[2],
                          db=argv[3])
     cur = db.cursor()
-    cursor.execute("SELECT cities.name FROM cities \
-    JOIN states ON cities.state_id = states.id WHERE states.name LIKE %s \
-    ORDER BY cities.id", (argv[4],))
-    rows = cursor.fetchall()
-    print(", ".join(city[0] for city in rows))
-    cursor.close()
+    SQL_query = """SELECT cities.id, cities.name, states.name
+                   FROM cities
+                   LEFT JOIN states
+                   ON cities.state_id = states.id
+                   ORDER BY cities.id ASC"""
+    cur.execute(SQL_query)
+
+    for row in cur.fetchall():
+        print(row)
     db.close()
