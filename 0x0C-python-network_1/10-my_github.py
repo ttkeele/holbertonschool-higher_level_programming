7da09f2e9df31d35ba2219a8b0cc639330de5f07
ttkeele <3264@holbertonschool.com>
2021-12-01 18:28:12 +0000

    lost ground went back to original commit

--- 0x0C-python-network_1/10-my_github.py
+++ 0x0C-python-network_1/10-my_github.py
@@ -6,8 +6,7 @@ to display your id
 if __name__ == '__main__':
     import requests
     from requests.auth import HTTPBasicAuth
-    from sys
-
-    auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
-    r = requests.get('https://api.github.com/user', auth=auth)
+    from sys import argv
+    r = requests.get('https://api.github.com/users/{}'.format(argv[1]),
+                     auth=HTTPBasicAuth(argv[1], argv[2]))
     print(r.json().get('id'))
