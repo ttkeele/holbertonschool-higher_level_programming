2568c6ea758d634d4e853974f0b7058cfdfca161
ttkeele <3264@holbertonschool.com>
2022-01-06 19:49:59 +0000

    spacing

--- 0x00-javascript-warm_up/7-multi_c.js
+++ 0x00-javascript-warm_up/7-multi_c.js
@@ -2,10 +2,10 @@
 if (process.argv[2] === undefined || isNaN(process.argv[2])) {
   console.log('Missing number of occurrences');
 } else {
-    const x = Number(process.argv[2]);
+  const x = Number(process.argv[2]);
     let i = 0;
     while (i < x) {
-	console.log('C is fun');
+  console.log('C is fun');
 	i++;
     }
 }
