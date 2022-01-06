efb16346fbd4b511b98b675ed349e87885ced3a1
ttkeele <3264@holbertonschool.com>
2022-01-06 19:59:58 +0000

    spacing

--- 0x00-javascript-warm_up/8-square.js
+++ 0x00-javascript-warm_up/8-square.js
@@ -2,10 +2,10 @@
 if (process.argv[2] === undefined || isNaN(process.argv[2])) {
   console.log('Missing size');
 } else {
-    const x = Number(process.argv[2]);
-    let i = 0;
-    while (i < x) {
-	console.log('X'.repeat(x));
-	i++;
+  const x = Number(process.argv[2]);
+  let i = 0;
+  while (i < x) {
+      console.log('X'.repeat(x));
+      it add i++;
     }
 }
