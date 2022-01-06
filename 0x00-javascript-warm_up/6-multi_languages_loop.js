31dd66e071fe3c27515ab7f99d1318b99676c580
ttkeele <3264@holbertonschool.com>
2022-01-06 19:27:52 +0000

    task 6

--- 0x00-javascript-warm_up/6-multi_languages_loop.js
+++ 0x00-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const s.len = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+const len = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (let i = 0; i < s; i++) {
     console.log(s[i]);
 }
