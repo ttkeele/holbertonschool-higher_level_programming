d4069abf6fe74d8414d88b404716eee14f956948
ttkeele <3264@holbertonschool.com>
2022-01-11 18:11:15 +0000

    forgot api

--- 0x02-javascript-web_scraping/3-starwars_title.js
+++ 0x02-javascript-web_scraping/3-starwars_title.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
 
 const request = require('request');
-
+const movie_id = process.argv[2];
+const url = 'https://swapi-api.hbtn.io/api/films/' + movie_id;
 request('http://swapi.co/api/films/' + process.argv[2], function (error, response, body) {
   if (error) {
     console.error(error);
