a42e66ddb9c341c5a109b6955a50832d23b84035
ttkeele <3264@holbertonschool.com>
2022-01-11 18:36:55 +0000

    task 3

--- 0x02-javascript-web_scraping/3-starwars_title.js
+++ 0x02-javascript-web_scraping/3-starwars_title.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 
 const request = require('request');
-const movie_id = process.argv[2];
-const url = 'https://swapi-api.hbtn.io/api/films/' + movie_id;
-request.get(url , function (error, response, body) {
+const movieId = process.argv[2];
+const url = 'https://swapi-api.hbtn.io/api/films/' + movieId;
+request.get(url, function (error, response, body) {
   if (error) {
     console.log(error);
   } else {
-      console.log(JSON.parse(body).title);
+    console.log(JSON.parse(body).title);
   }
 });
