cc36df733dd7322238b9047d23d8ff4d4773c925
ttkeele <3264@holbertonschool.com>
2022-01-11 18:15:49 +0000

    changed syntax

--- 0x02-javascript-web_scraping/3-starwars_title.js
+++ 0x02-javascript-web_scraping/3-starwars_title.js
@@ -5,7 +5,7 @@ const movie_id = process.argv[2];
 const url = 'https://swapi-api.hbtn.io/api/films/' + movie_id;
 request.get(url , function (error, response, body) {
   if (error) {
-    console.error(error);
+    console.log(error);
   }
   console.log(JSON.parse(body).title);
 });
