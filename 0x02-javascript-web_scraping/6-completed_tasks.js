a59d8d89ef729cf420f3b1b06ac56586ad8b00c5
ttkeele <3264@holbertonschool.com>
2022-01-11 18:53:01 +0000

    adding an s

--- 0x02-javascript-web_scraping/6-completed_tasks.js
+++ 0x02-javascript-web_scraping/6-completed_tasks.js
@@ -6,9 +6,9 @@ request.get(url, function (error, response, body) {
   if (error) {
     console.log(error);
   } else {
-    const todo = JSON.parse(body);
+    const todos = JSON.parse(body);
     const completed = {};
-    for (const i of todo) {
+    for (const i of todos) {
       if (i.completed === true) {
         if (i.userId in completed) {
 	  completed[i.userId]++;
