330cd2b90832aa8005ec2ac405a098760ce43fda
ttkeele <3264@holbertonschool.com>
2022-01-11 21:30:18 +0000

    forgot the i

--- 0x03-javascript-web_jquery/7-script.js
+++ 0x03-javascript-web_jquery/7-script.js
@@ -1,4 +1,4 @@
 const $ = window.$;
-$.get('https://swap-api.hbtn.io/api/people/5/?format=json', function (data) {
+$.get('https://swapi-api.hbtn.io/api/people/5/?format=json', function (data) {
     $('DIV#character').text(data.name);
 });
