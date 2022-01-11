775e59997870d2590d6b82aeccb9622dbf037b16
ttkeele <3264@holbertonschool.com>
2022-01-11 21:32:35 +0000

    task 8

--- 0x03-javascript-web_jquery/8-script.js
+++ 0x03-javascript-web_jquery/8-script.js
@@ -1,5 +1,5 @@
 const $ = window.$;
-$.get('https://swapi.co/api/films/?format=json', function (data) {
+$.get('https://swapi-api.hbtn.io/api/films/?format=json', function (data) {
     $.each(data.results, function (index, value) {
 	$('<li>' + value.title + '</li>').appendTo('UL#list_movies');
     });
