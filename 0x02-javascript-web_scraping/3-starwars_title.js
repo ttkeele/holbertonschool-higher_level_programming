03646545fe0760196f2d2d05d2e2146af9867f16
ttkeele <3264@holbertonschool.com>
2022-01-11 18:14:34 +0000

    url const

--- 0x02-javascript-web_scraping/3-starwars_title.js
+++ 0x02-javascript-web_scraping/3-starwars_title.js
@@ -3,7 +3,7 @@
 const request = require('request');
 const movie_id = process.argv[2];
 const url = 'https://swapi-api.hbtn.io/api/films/' + movie_id;
-request('http://swapi.co/api/films/' + process.argv[2], function (error, response, body) {
+request.get(url , function (error, response, body) {
   if (error) {
     console.error(error);
   }
