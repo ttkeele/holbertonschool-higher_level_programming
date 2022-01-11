73d259541393760424c5b7e3fb3d549771fd95b6
ttkeele <3264@holbertonschool.com>
2022-01-11 15:37:03 +0000

    fixed module

--- 0x02-javascript-web_scraping/1-writeme.js
+++ 0x02-javascript-web_scraping/1-writeme.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
-const rm = require('rm');
+const fs = require('fs');
 
 try {
-  rm.writeFile(process.argv[2], process.argv[3], 'utf8', function (err, result) { if (err) console.log(err); });
+  fs.writeFile(process.argv[2], process.argv[3], 'utf8', function (err, result) { if (err) console.log(err); });
 } catch (err) {
   console.log(err);
 }
