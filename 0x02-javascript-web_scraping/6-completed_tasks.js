4e7109ed0e664be2c552f07605e2f85a4c6f738e
ttkeele <3264@holbertonschool.com>
2022-01-11 18:37:05 +0000

    task 6

--- 0x02-javascript-web_scraping/6-completed_tasks.js
+++ 0x02-javascript-web_scraping/6-completed_tasks.js
@@ -3,20 +3,20 @@
 const request = require('request');
 const url = process.argv[2];
 request.get(url, function (error, response, body) {
-    if (error) {
-	console.log(error);
+  if (error) {
+    console.log(error);
+  } else {
+    const todo = JSON.parse(body);
+    const completed = {};
+    for (const i of todo) {
+      if (i.completed === true) {
+        if (i.usrId in completed) {
+	  completed[i.usrId]++;
+        } else {
+	  completed[i.usrId] = 1;
+        }
+      }
     }
-    const dict = JSON.parse(body).reduce((acc, elem) => {
-	if (!acc[elem.userId]) {
-	    if (elem.completed) {
-		acc[elem.userId] = 1;
-	    }
-	} else {
-	    if (elem.completed) {
-		acc[elem.userId] += 1;
-	    }
-	}
-	return acc;
-    }, {});
-    console.log(dict);
+    console.log(completed);
+  }
 });
