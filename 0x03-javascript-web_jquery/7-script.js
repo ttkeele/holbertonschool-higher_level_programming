c516b21c72dd8b98808f5d7e9209fb47f5c5b46c
ttkeele <3264@holbertonschool.com>
2022-01-11 21:27:35 +0000

    typo

--- 0x03-javascript-web_jquery/7-script.js
+++ 0x03-javascript-web_jquery/7-script.js
@@ -1,4 +1,4 @@
 const $ = window.$;
-$.get('https://swapi.co/api/people/5/?format=json', function (data) {
+$.get('https://swap-api.hbtn.io/api/people/5/?format=json', function (data) {
     $('DIV#character').text(data.name);
 });
