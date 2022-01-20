e892dc77098b7d2f80660dae12461d3a3d84cbbc
ttkeele <3264@holbertonschool.com>
2022-01-20 16:30:28 +0000

    Public instance method: def area(self): that raises an Exception with the message area() is not implemented

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+creates class
+"""
+
+
+class BaseGeometry:
+    """class with public instance method"""
+
+    def area(self):
+        """raises an Exception with the message
+        'area() is not implemented'
+        """
+
+        raise Exception('area() is not implemented')
