a0cb127ce21c2c06df71fe9c6af3647faafefd3e
ttkeele <3264@holbertonschool.com>
2022-01-20 16:22:06 +0000

    task1

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -7,5 +7,5 @@ creates class inheriting from the list class
 class MyList(list):
     """class MyList inherits from list, sorted"""
     def print_sorted(self):
-        """prints list in order"""
+        
         print(sorted(self))
