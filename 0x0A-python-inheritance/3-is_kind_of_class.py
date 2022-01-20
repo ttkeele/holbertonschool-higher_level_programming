c9039cb0ee21aa5ecfba8779c5113c31916fe0c1
ttkeele <3264@holbertonschool.com>
2022-01-20 15:51:43 +0000

    task3

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+function that returns True if the object is an instance of a class
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """ returns true or false """
+
+
+    if isinstance(obj, a_class):
+        return False
+    return True
