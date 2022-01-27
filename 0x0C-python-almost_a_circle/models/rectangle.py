16fdcd4da7c0a4402a8d7a3d44c497782ee08452
ttkeele <3264@holbertonschool.com>
2022-01-27 21:24:54 +0000

    maybe

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -7,6 +7,7 @@ from models.base import Base
 
 class Rectangle(Base):
     """rectangle class inherits from base"""
+
     def __init__(self, width, height, x=0, y=0, id=None):
         """initilizes the rectagle
 
@@ -17,6 +18,23 @@ class Rectangle(Base):
             y: horizontal location of rectangle
             id: id for rectangle
         """
+        if type(width) is not int:
+            raise TypeError("width must be an integer")
+        if width <= 0:
+            raise ValueError("width must be > 0")
+        if type(height) is not int:
+            raise TypeError("height must be an integer")
+        if height <= 0:
+            raise ValueError("height must be > 0")
+        if type(x) is not int:
+            raise TypeError("x must be an integer")
+        if x < 0:
+            raise ValueError("x must be >= 0")
+        if type(y) is not int:
+            raise TypeError("y must be an integer")
+        if y < 0:
+            raise ValueError("y must be >= 0")
+        
 
         super().__init__(id)
         self.__width = width
