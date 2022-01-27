608987ce84c003ccc2b61330c0a7b7d5743ba08c
ttkeele <3264@holbertonschool.com>
2022-01-27 19:40:26 +0000

    first class Base

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""module that contains base class"""
+
+
+class Base:
+    """base class"""
+    __nb_objects = 0
+
+    def __ini__(self, id=None):
+        """initailizes object"""
+
+        Arg:
+            id(int): id to assign
+        """
+        if id is None:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+        else:
+            self.id = id
