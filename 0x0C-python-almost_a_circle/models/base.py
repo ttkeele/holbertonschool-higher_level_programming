740ba945fa4091a44fc1fb0ebab153d003151e90
ttkeele <3264@holbertonschool.com>
2022-01-27 20:44:07 +0000

    base model

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """module that contains base class"""
 
+import json
 
 class Base:
     """base class"""
@@ -12,3 +13,56 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """list of dictionaries -> json format"""
+        if list_dictionaries is None or len(list_dictionaries) < 1:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """saves json format from list_objs to type.json file"""
+        name = cls.__name__ + ".json"
+        with open(name, "w+") as f:
+            if list_objs is None or len(list_objs) == 0:
+                f.write("[]")
+            else:
+                lst = []
+                for o in list_objs:
+                    lst.append(o.to_dictionary())
+                f.write(cls.to_json_string(lst))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """returns obj from json_string"""
+        if json_string is None:
+            return []
+        else:
+            return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """creates a new object from given info"""
+        if cls.__name__ == "Rectangle":
+            dump = cls(1, 1, 0, 0)
+        if cls.__name__ == "Square":
+            dump = cls(1, 0, 0)
+        dump.update(**dictionary)
+        return dump
+
+    @classmethod
+    def load_from_file(cls):
+        """returns object from data stored in json format in type.json file"""
+        filename = cls.__name__ + ".json"
+        objlist = []
+        try:
+            with open(filename, "r") as f:
+                jsonlist = cls.from_json_string(f.read())
+                for j in jsonlist:
+                    objlist.append(cls.create(**j))
+                    return objlist
+        except FileNotFoundError:
+            return []
